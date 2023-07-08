97a82fbada9600676a9d3ac025f15f4241eecfe4
EdsonMFN <edsonfernandes.n@outlook.com>
2023-07-08 17:40:18 -0300

    COMMIT

--- src/main/java/desafioTecnico/api/service/AssociadoService.java
+++ src/main/java/desafioTecnico/api/service/AssociadoService.java
@@ -1,16 +1,12 @@
 package desafioTecnico.api.service;
 
-import desafioTecnico.api.controller.response.ResponseAssociado;
-import desafioTecnico.api.controller.resquest.RequestAssociado;
-import desafioTecnico.api.entity.associado.Associado;
-import desafioTecnico.api.entity.voto.Voto;
+
 import desafioTecnico.api.repository.RepositoryAssociado;
 import desafioTecnico.api.repository.RepositoryVoto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class AssociadoService {
@@ -23,49 +19,5 @@ public class AssociadoService {
     @Autowired
     private RepositoryVoto repositoryVoto;
 
-<<<<<<< HEAD
-    public ResponseAssociado criarAssociado(RequestAssociado requestAssociado){
 
-        Associado associado = new Associado();
-        associado.setNome(requestAssociado.getNome());
-        associado.setCpf(requestAssociado.getCpf());
-        associado = repositoryAssociado.save(associado);
-=======
-    public ResponseAssociado criarAssociado(Long idVoto, RequestAssociado requestAssociado, ResponseAssociado responseAssociado){
-
-        Optional<Voto> voto = repositoryVoto.findById(idVoto);
-        Associado associado = new Associado();
-        associado.setNome(requestAssociado.getNome());
-        associado.setCpf(requestAssociado.getCpf());
-        associado.setVoto(voto.get());
->>>>>>> origin/master
-
-        return new ResponseAssociado(associado);
-    }
-
-    public List<ResponseAssociado> listarAssociado(){
-
-        List<Associado> associados = repositoryAssociado.findAll();
-
-        return associados.stream().map(ResponseAssociado::new).toList();
-    }
-
-    public ResponseAssociado alterarAssociado(RequestAssociado requestAssociado){
-
-        Associado associado = new Associado();
-        associado.setNome(requestAssociado.getNome());
-        associado.setCpf(requestAssociado.getCpf());
-<<<<<<< HEAD
-        associado = repositoryAssociado.save(associado);
-=======
->>>>>>> origin/master
-
-        return new ResponseAssociado(associado);
-    }
-
-    public void deletarAssociado(Long idAssociado){
-
-       repositoryAssociado.deleteById(idAssociado);
-
-    }
 }
